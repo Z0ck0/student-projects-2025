250e043d897aeba9e71402a3df03f494f3a66fba
Zoran Dimitrievski <132205377+Z0ck0@users.noreply.github.com>
2023-11-15 20:35:38 +0100

    Add CredentialsGenerator in Utils with methods for generating random email, password, first name and last name Introduce DateTimeManagementUtils with methods for date and time management

---
+++ src/test/java/com/demoqa/utilities/DateTimeManagementUtils.java
@@ -0,0 +1,59 @@
+package com.demoqa.utilities;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTimeManagementUtils {
+
+    // Date format used for formatting date/time strings
+    private static final String DEFAULT_DATE_FORMAT = "dd-MM-yyyy HH:mm:ss";
+
+    // Calendar instance used for date/time calculations
+    private static final Calendar CALENDAR = Calendar.getInstance();
+
+    private DateTimeManagementUtils() {
+        // Private constructor to prevent instantiation
+    }
+
+
+    //region    Get the current timestamp as a formatted string.
+    public static String getCurrentTimestamp() {
+        return formatTimestamp(new Date());
+    }
+    //endregion
+
+
+    //region    Format a given date as a string using the default date format.
+    public static String formatTimestamp(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
+        return dateFormat.format(date);
+    }
+    //endregion
+
+
+    //region    Add a specified number of days to a given date.
+    public static Date addDaysToDate(Date date, int daysToAdd) {
+        CALENDAR.setTime(date);
+        CALENDAR.add(Calendar.DAY_OF_MONTH, daysToAdd);
+        return CALENDAR.getTime();
+    }
+    //endregion
+
+
+    //region    Get the hour of the day from a given date.
+    public static int getHourOfDay(Date date) {
+        CALENDAR.setTime(date);
+        return CALENDAR.get(Calendar.HOUR_OF_DAY);
+    }
+    //endregion
+
+
+    //region    Get the minute of the hour from a given date.
+    public static int getMinuteOfHour(Date date) {
+        CALENDAR.setTime(date);
+        return CALENDAR.get(Calendar.MINUTE);
+    }
+    //endregion
+}
+
